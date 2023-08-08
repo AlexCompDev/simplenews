e3a7dca1286bf9bf8d87eb78eea79b8ecdca0edd
AlexCompDev <alexcompanyru@gmail.com>
2023-08-08 10:43:34 +0300

    Добавил форму, закомментировал лишние строки

---
+++ news/forms.py
@@ -0,0 +1,27 @@
+from .models import Articles
+from django.forms import ModelForm, TextInput, Textarea
+
+class ArticlesForm(ModelForm):
+    class Meta:
+        model = Articles
+        fields = ['title', 'announce', 'full_text']
+
+        widgets = {
+            "title": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Название статьи'
+            }),
+            "announce": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Анонс статьи'
+            }),
+            "announce": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Анонс статьи'
+            }),
+            "full_text": Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Текст статьи'
+            }),
+
+        }
